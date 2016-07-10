5fde82567a52bb32fdbe8572cb7ef57dbecacd9f
Mr.Q <qianlishun@mrq-macbook-pro.local>
2016-07-10 09:50:36 +0800

    分类

---
+++ NewDemo/UIView+QLSFrame.h
@@ -0,0 +1,20 @@
+//
+//  UIView+HMCategory.h
+//  
+//
+//  Created by AndyDev on 15/9/5.
+//  Copyright © 2015年 AndyDev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (QLSFrame)
+@property (nonatomic, assign) CGFloat x;
+@property (nonatomic, assign) CGFloat y;
+@property (nonatomic, assign) CGFloat width;
+@property (nonatomic, assign) CGFloat height;
+@property (nonatomic, assign) CGFloat centerX;
+@property (nonatomic, assign) CGFloat centerY;
+@property (nonatomic, assign) CGSize size;
+@property (nonatomic, assign) CGPoint origin;
+@end
